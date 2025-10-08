b53018b3b235ffa080aec4b6f7b85628899a7e73
just_lofe <lofikpc@gmail.com>
2025-10-09 01:41:16 +0700

    fixup FileTest.java

--- src/test/java/su/windmill/bytes/FileTest.java
+++ src/test/java/su/windmill/bytes/FileTest.java
@@ -1,31 +1,25 @@
-package su.windmill.bytes.examples;
+package su.windmill.bytes;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import su.windmill.bytes.FastBytes;
 import su.windmill.bytes.buffer.FastBuffer;
 
 import java.io.File;
+import java.net.URISyntaxException;
 
-public class Example2Test {
+public class FileTest {
 
     @Test
-    public void test() {
+    public void test() throws URISyntaxException {
         FastBuffer buffer = FastBytes.expanding();
 
         buffer.writeInt(42);
         buffer.writeFloat(24.421f);
 
-        File file;
-        try {
-            file = new File(
-                    new File(Example2Test.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile(),
-                    "test.bin"
-            );
-        }
-        catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        File file = new File(
+                new File(FileTest.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile(),
+                "test.bin"
+        );
 
         FastBytes.writeFile(file, buffer);
 
